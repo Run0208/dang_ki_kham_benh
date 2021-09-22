2a49814c3fdc5cc607b4546ed665aca948c6f04a
Duong Dang Duc <duongdangduc02082000@gmail.com>
2021-09-22 22:34:32 +0700

    !data.email ||

--- src/router/web.js
+++ src/router/web.js
@@ -2,6 +2,7 @@ import express from "express";
 import homeController from "../controllers/homeController";
 import userContrller from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
+import patientController from "../controllers/patientController";
 
 let router = express.Router();
 
@@ -33,6 +34,7 @@ let initWebRoutes = (app) => {
     router.get('/api/get-profile-doctor-by-id', doctorController.getProfileDoctor);
 
 
+    router.post('/api/patient-book-appointment', patientController.postBookAppointment);
 
     return app.use("/", router);
 }
